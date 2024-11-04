1bfa525cb816051541e686e965d5f8680f0aeab0
Alybee75 <alyana_erin_bondoc@dlsu.edu.ph>
2024-11-04 17:54:41 +0800

    verify

--- client/index.js
+++ client/index.js
@@ -6,9 +6,16 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  let merkleTree = new MerkleTree(niceList);
+
+  const nameToProve = niceList[0];
+
+  const proof = merkleTree.getProof(niceList.indexOf(nameToProve));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: nameToProve,
+    proof: proof,
   });
 
   console.log({ gift });
